a1bd352abbaebd83d65bb74235bb9dd55189cc91
harry1050732955 <harry1050732955@gmail.com>
2021-02-12 16:29:30 +0800

    搜索页面返回上一页 搜索页面类型切换

--- src/views/search/search.vue
+++ src/views/search/search.vue
@@ -1,15 +1,147 @@
 <template>
-    <div>
-
+  <div id="search">
+    <div class="search_header">
+      <div class="centerimg" @click="gotoback()">
+        <img :src="backimg" alt="" class="back_img" />
+      </div>
+      <div class="search_header_div">
+        <div class="search_input_div">
+          <div class="searchtype">
+            <div>
+                {{ searchType }}
+            </div>
+            <img src="../../assets/img/search/down@2x.png" alt="" class="seledown" @click="showsearch()">
+          </div>
+          <input
+            type="text"
+            maxlength="20"
+            placeholder="请输入关键字"
+            v-model="searchkeywordk"
+            class="search_input"
+          />
+          <div class="centerimg">
+            <img :src="searchimg" alt="" class="search_img" />
+          </div>
+        </div>
+        <div class="search_sele" v-show="showsearchtyoeList">
+            <div v-for="(item,index) in searchTypeList" :key="index" @click="changeSearchType(item)">
+                {{item.name}}
+            </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        
+export default {
+  data() {
+    return {
+      backimg: require("../../assets/img/search/back@2x.png"),
+      searchimg: require("../../assets/img/search/search2@2x.png"),
+      searchkeywordk: "",
+      searchType: "资讯",
+      searchTypeList: [
+        {
+          name: "咨询",
+          type: 1,
+        },
+        {
+          name: "贴子",
+          type: 2,
+        },
+        {
+          name: "用户",
+          type: 3,
+        },
+      ],
+      showsearchtyoeList:false
+    };
+  },
+  methods: {
+    gotoback() {
+      //返回上一个页面
+      window.history.go(-1);
+    },
+    showsearch(){//显示搜索类型
+         this.showsearchtyoeList = true;
+    },
+    changeSearchType(type){//切换搜索类型
+        this.showsearchtyoeList = false;
+        this.searchType = type.name
     }
+  },
+};
 </script>
 
 <style lang="less" scoped>
-    
+#search {
+}
+.centerimg {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.search_header {
+  display: flex;
+  align-items: center;
+  height: 88px;
+  background-color: #505050;
+  .back_img {
+    width: 30px;
+    height: 28px;
+    margin: 0 30px;
+  }
+  .search_header_div {
+    display: flex;
+    align-items: center;
+    margin-left: 25px;
+    position:relative;
+  }
+  .search_img {
+    width: 46px;
+    height: 46px;
+  }
+  .search_input_div {
+    display: flex;
+    align-items: center;
+    color: #fff;
+    border: 1px solid #666;
+    height: 58px;
+    padding: 0 27px;
+    border-radius: 27px;
+  }
+  .searchtype{
+      display: flex;
+      align-items: center;
+      border-right: 1px solid #fff;
+      font-size: 18px;
+  }
+  .search_input {
+    color: #fff;
+    background: transparent;
+    border: none;
+  }
+  .search_input input::-webkit-input-placeholder {
+    color: #999;
+    font-size: 12px;
+  }
+  .seledown{
+      width: 21px;
+      height: 12px;
+      margin: 0 10px;
+  }
+}
+.search_sele{
+    position: absolute;
+    top: 50px;
+    left: 20px;
+    color: #fff;
+    font-size: 18px;
+    background-color: #666;
+    div{
+        padding: 5px 10px;
+        border: 1px solid #999;
+    }
+}
 </style>
